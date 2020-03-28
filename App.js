dbae725f3a2a1f40efa21fc827eaae8a3ab3f784
devagreen9 <61478502+devagreen9@users.noreply.github.com>
2020-03-28 19:18:44 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: 'purple',
+      clickCounter: 0
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({
+      color: 'orange',
+      clickCounter: this.state.clickCounter + 1
+    })
+    console.log('click counter ===>', this.state.clickCounter)
+  }
+
+  onNextChange = (e) => {
+    this.setState({
+      color: 'purple',
+      clickCounter: this.state.clickCounter + 1
+    })
+    console.log('click counter ===>', this.state.clickCounter)
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Box Color Change</h1>
+        <div className='colorBox' style={{backgroundColor: this.state.color}}
+        
+        >
+        </div>
+        <button onClick={this.onChange} >Click Me!</button>
+        <button onClick={this.onNextChange}>No! Click Me!</button>
+      </div>
+    )
+  }
+}
