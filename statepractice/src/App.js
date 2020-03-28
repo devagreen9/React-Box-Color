cea0d28298b41ca46f37e63480ad56f4b3ce7699
Angel Juarez <angeljuarez729@gmail.com>
2020-03-28 12:53:29 -0400

    first commit

---
+++ statepractice/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: 'purple',
+      clickCounter: 0
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({
+      color: 'orange',
+      clickCounter: this.state.clickCounter++
+    })
+    console.log('click counter ===>', this.state.clickCounter)
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Box Color Change</h1>
+        <div className='colorBox' style={{backgroundColor: this.state.color}}
+        onClick={this.onChange}
+        >
+          <p>Click Me!</p>
+        </div>
+      </div>
+    )
+  }
+}
